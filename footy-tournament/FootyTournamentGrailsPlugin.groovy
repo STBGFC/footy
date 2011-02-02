faf40f3ed05fd8ecf192e365dfff2988281d869e
Darren Davison <darren@davisononline.org>
2011-02-02 21:00:27 +0000

    allow latest version of webflow

--- footy-tournament/FootyTournamentGrailsPlugin.groovy
+++ footy-tournament/FootyTournamentGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class FootyTournamentGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ['footy-core': 1.0, paypal: 0.6, webflow: '1.3.6']
+    def dependsOn = ['footy-core': 1.0, paypal: 0.6, webflow: "1.3 > *"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
