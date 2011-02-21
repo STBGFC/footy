aeec7ccae3f84452b99174dab32b8f86ec5e1455
Darren Davison <darren@davisononline.org>
2011-02-21 19:58:04 +0000

    Revert "constraints and testing in RegistraionController"
    This reverts commit 87ae1404dbaa7431d7c932a704bad962d11733c1.

--- footy-registration/test/unit/org/davisononline/footy/registration/RegistrationControllerTests.groovy
+++ footy-registration/test/unit/org/davisononline/footy/registration/RegistrationControllerTests.groovy
@@ -1,7 +1,6 @@
 package org.davisononline.footy.registration
 
 import grails.test.*
-import org.davisononline.footy.core.Address
 
 class RegistrationControllerTests extends ControllerUnitTestCase {
     protected void setUp() {
@@ -12,65 +11,7 @@ class RegistrationControllerTests extends ControllerUnitTestCase {
         super.tearDown()
     }
 
-    void testPlayerCommandConstraints() {
-        mockForConstraintsTests(PlayerCommand)
-        def p = new PlayerCommand(
-            givenName: 'Fred',
-            familyName: 'Bassett',
-            dob: new Date(1970, 0, 20)
-        )
-        assertTrue p.validate()
-
-        p.givenName = null
-        assertFalse p.validate()
-        p.givenName = ''
-        assertFalse p.validate()
-        p.givenName = 'Fredxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-        assertFalse p.validate()
-        p.givenName = 'Fred'
-        assertTrue p.validate()
-
-        p.familyName = null
-        assertFalse p.validate()
-        p.familyName = ''
-        assertFalse p.validate()
-        p.familyName = 'Bassett'
-        assertTrue p.validate()
-    }
-
-    void testPersonCommandConstraints() {
-        mockForConstraintsTests(PersonCommand)
-        def p = new PersonCommand(
-                givenName: 'Fred',
-                familyName: 'Bassett',
-                email:"foo@bar.com",
-                phone1: '0876544332',
-                address: Address.parse("123 Some St., GU1 1DB")
-        )
-        assertTrue p.validate()
-
-        // givenName and familyName constraints are tested above
-
-        p.email = ''
-        assertFalse p.validate()
-        p.email = 'foo@bar.com'
-        assertTrue p.validate()
-
-        p.phone1 = ''
-        assertFalse p.validate()
-        p.phone1 = '01234566789'
-        assertTrue p.validate()
-
-    }
-
-    void testPlayerCommandAgeCalculation() {
-        mockForConstraintsTests(PlayerCommand)
-        def p = new PlayerCommand(
-            givenName: 'Fred',
-            familyName: 'Bassett',
-            dob: new Date(2003, 0, 20)
-        )
-        // TODO: make cutoff configurable
+    void testSomething() {
 
     }
 }
