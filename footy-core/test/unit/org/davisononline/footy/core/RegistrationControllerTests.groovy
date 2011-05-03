5fece15fdf7cf3bdebe88a721974c6ae307353eb
Darren Davison <darren@davisononline.org>
2011-05-03 13:17:15 +0100

    cleanup tests

--- footy-core/test/unit/org/davisononline/footy/core/RegistrationControllerTests.groovy
+++ footy-core/test/unit/org/davisononline/footy/core/RegistrationControllerTests.groovy
@@ -11,43 +11,4 @@ class RegistrationControllerTests extends ControllerUnitTestCase {
     protected void tearDown() {
         super.tearDown()
     }
-
-    void testPersonCommandConstraints() {
-        mockForConstraintsTests(PersonCommand)
-        def p = new PersonCommand(
-                givenName: 'Fred',
-                familyName: 'Bassett',
-                email:"foo@bar.com",
-                phone1: '0876544332',
-                address: Address.parse("123 Some St., GU1 1DB")
-        )
-        assertTrue p.validate()
-
-        // givenName and familyName constraints are tested above
-
-        p.email = ''
-        assertFalse p.validate()
-        p.email = 'foo@bar.com'
-        assertTrue p.validate()
-
-        p.phone1 = ''
-        assertFalse p.validate()
-        p.phone1 = '01234566789'
-        assertTrue p.validate()
-
-    }
-
-    void testToPerson() {
-        def p = new PersonCommand(
-                givenName: 'Fred',
-                familyName: 'Bassett',
-                email:"foo@bar.com",
-                phone1: '0876544332',
-                address: Address.parse("123 Some St., GU1 1DB")
-        )
-        def person = p.toPerson()
-        assertEquals 'Fred', person.givenName
-        assertEquals 'Bassett', person.familyName
-        assertEquals 'foo@bar.com', person.email
-    }
 }
