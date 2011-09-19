60a81b22a0d9bb2bccec4358ebd2bf85c58c5824
Darren Davison <darren@davisononline.org>
2011-09-19 20:57:19 +0100

    add config elements to binding to templates can use them.

--- footy-core/src/groovy/org/davisononline/footy/core/utils/TemplateUtils.groovy
+++ footy-core/src/groovy/org/davisononline/footy/core/utils/TemplateUtils.groovy
@@ -1,5 +1,7 @@
 package org.davisononline.footy.core.utils
 
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
 /**
  * attempting to read a GString in from external sources has issues
  * so this class acts as a helper to manage (for example) email
@@ -9,6 +11,7 @@ class TemplateUtils {
 
     static def eval(String text, binding) {
         def engine = new groovy.text.SimpleTemplateEngine()
+        binding.cfg = ConfigurationHolder.config
         def template = engine.createTemplate(text).make(binding)
         template.toString()
     }
