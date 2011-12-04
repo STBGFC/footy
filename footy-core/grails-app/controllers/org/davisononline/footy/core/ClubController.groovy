ffa2d936193e6908c59535ee113d95c75442cc64
Darren Davison <darren@davisononline.org>
2011-12-04 02:37:27 +0000

    add download of all parent details (VCF) for a CLUB_ADMIN

---
+++ footy-core/grails-app/controllers/org/davisononline/footy/core/ClubController.groovy
@@ -0,0 +1,20 @@
+package org.davisononline.footy.core
+
+import grails.plugins.springsecurity.Secured
+
+class ClubController {
+
+    def index = { }
+
+    @Secured(["ROLE_CLUB_ADMIN"])
+    def addresscards = {
+        response.setHeader("Content-disposition", "attachment;filename=${Club.homeClub.name.replace(" ", "_")}_all_contacts.vcf")
+        def contacts = Person.findAllByEligibleParent(true)
+        render (
+            template: '/team/vcard',
+            plugin: 'footy-core',
+            collection: contacts,
+            contentType: 'text/x-vcard'
+        )
+    }
+}
