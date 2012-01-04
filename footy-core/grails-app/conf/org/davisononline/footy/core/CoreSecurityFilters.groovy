f359d1d56e9c0a88f0be955b4809de0ac1bb7d01
Darren Davison <darren@davisononline.org>
2012-01-04 00:19:16 +0000

    restrict ROLE_COACH functions to "own team only" behaviour

---
+++ footy-core/grails-app/conf/org/davisononline/footy/core/CoreSecurityFilters.groovy
@@ -0,0 +1,37 @@
+package org.davisononline.footy.core
+
+/**
+ * attempt to prevent coaches performing ROLE_COACH activities for teams that
+ * they are not managers of
+ */
+class CoreSecurityFilters {
+
+    def footySecurityService
+
+    def filters = {
+        playerPersonEdit(controller: '(player|person)', action: '(edit|update)') {
+            before = {
+                def allowed
+                if (controllerName == 'player')
+                    allowed = footySecurityService.canEditPlayer(Player.get(params.id))
+                else
+                    allowed = footySecurityService.canEditPerson(Person.get(params.id))
+
+                if (!allowed) {
+                    response.sendError 403
+                    return false
+                }
+            }
+        }
+
+        newsAdd(controller: 'team', action: 'addNews') {
+            before = {
+                if (!footySecurityService.isAuthorisedForManager(params['team.id'])) {
+                    response.sendError 403
+                    return false
+                }
+            }
+        }
+    }
+
+}
