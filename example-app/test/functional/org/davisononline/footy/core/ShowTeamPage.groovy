96bd445ccdca37fd9d125951b10d74d52a33aa49
Darren Davison <darren@davisononline.org>
2012-01-08 21:26:41 +0000

    more func test updates

---
+++ example-app/test/functional/org/davisononline/footy/core/ShowTeamPage.groovy
@@ -0,0 +1,31 @@
+package org.davisononline.footy.core
+
+import org.davisononline.footy.*
+
+class ShowTeamPage extends FootyPage {
+
+    static at = { 
+        $('div#iconbar').present()
+    }
+
+    static content = {
+
+        changeSponsor(required: false) { $('a', text: 'Add/Change Sponsor') }
+        sendEmailButton(required: false) { $('a', title: 'Send Email Message') }
+        addNewsButton(required: false) { $('a', title: 'Create News Item') }
+
+        newsForm(required: false) { $('form#news') }
+        newsSubmitButton(required: false) { $('input', value: 'Create') }
+                
+    }
+
+    void addNews(subject, body, global = false) {
+        addNewsButton.click()
+        Thread.sleep(2000) // message box animation delay
+        newsForm.subject = subject
+        newsForm.body = body
+        if (global) newsForm.clubWide.click()
+        newsSubmitButton.click()
+    }
+}
+
---
+++ example-app/test/functional/org/davisononline/footy/core/TeamTests.groovy
@@ -0,0 +1,11 @@
+package org.davisononline.footy.core
+
+import org.davisononline.footy.*
+
+class TeamTests extends AbstractTestHelper {
+
+    void testTeam() {
+        true
+    }
+}
+
