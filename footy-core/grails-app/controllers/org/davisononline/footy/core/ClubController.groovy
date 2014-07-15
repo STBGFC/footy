16b5405984065cae4caa4b8655bcdbd2100b7fff
Darren Davison <darren@davisononline.org>
2014-07-15 22:30:56 +0100

    Updates the address card list to omit persons without an address (ie tournament registrants)

--- footy-core/grails-app/controllers/org/davisononline/footy/core/ClubController.groovy
+++ footy-core/grails-app/controllers/org/davisononline/footy/core/ClubController.groovy
@@ -9,7 +9,7 @@ class ClubController {
     @Secured(["ROLE_CLUB_ADMIN"])
     def addresscards = {
         response.setHeader("Content-disposition", "attachment;filename=${Club.homeClub.name.replace(" ", "_")}_all_contacts.vcf")
-        def contacts = Person.findAllByEligibleParent(true)
+        def contacts = Person.findAllByEligibleParentAndAddressIsNotNull(true)
         render (
             template: '/team/vcard',
             plugin: 'footy-core',
